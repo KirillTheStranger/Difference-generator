af6761a7c7b0cdd57548c2e34fe81abcb1363e03
Kirill <faeeeee@gmail.com>
2023-09-28 13:12:55 +0500

    rework path

--- gendiff.js
+++ gendiff.js
@@ -10,9 +10,9 @@ program
   .version('1.0.0')
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
-  .action((path1, path2) => {
-    console.log(fs.readFileSync(path1, 'utf8'));
-    console.log(fs.readFileSync(path2, 'utf8'));
+  .action((filepath1, filepath2) => {
+    console.log(fs.readFileSync(filepath1, 'utf8'));
+    console.log(fs.readFileSync(filepath2, 'utf8'));
   });
 
 program.parse();
