314f76b5f523c81aedb0529ebceaaa6f44aeed70
Kirill <faeeeee@gmail.com>
2023-10-02 14:47:09 +0500

    add test file

---
+++ __test__/gendiff.test.js
@@ -0,0 +1,5 @@
+import { parseFromPath, getFixturePath } from '../helpers.js';
+
+const testFile1 = parseFromPath(getFixturePath('test_file1.json'));
+const testFile2 = parseFromPath(getFixturePath('test_file2.json'));
+const testFile3 = parseFromPath(getFixturePath('test_file3.json'));
