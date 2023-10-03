1bdfa3c7d5b7c253e555e5ee004a24dc30e91b72
Kirill <faeeeee@gmail.com>
2023-10-03 12:44:38 +0500

    fix linter errors

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 
-import mainLogic from '../index.js';
+import { program } from 'commander';
+import genDiff from '../index.js';
 
-mainLogic();
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action(genDiff);
+
+program.parse();
--- index.js
+++ index.js
@@ -1,59 +1,49 @@
-import { program } from 'commander';
 import { parseFromPath } from './helpers.js';
 
-const mainLogic = () => {
-  program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .option('-f, --format <type>', 'output format')
-  .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    const file1 = parseFromPath(filepath1);
-    const file2 = parseFromPath(filepath2);
+const genDiff = (filepath1, filepath2) => {
+  const file1 = parseFromPath(filepath1);
+  const file2 = parseFromPath(filepath2);
 
-    const keys1 = Object.keys(file1);
-    const keys2 = Object.keys(file2);
+  const keys1 = Object.keys(file1);
+  const keys2 = Object.keys(file2);
 
-    const sortedKeys = [...keys1, ...keys2]
-      .reduce((acc, key) => {
-        if (!acc.includes(key)) {
-          acc.push(key);
-        }
-        return acc;
-      }, [])
-      .sort((a, b) => {
-        if (a.toLowerCase() < b.toLowerCase()) {
-          return -1;
-        }
-        if (a.toLowerCase() > b.toLowerCase()) {
-          return 1;
-        }
-        return 0;
-      });
-
-    const resultedArr = sortedKeys.reduce((acc, key) => {
-      if (file1[key] === file2[key]) {
-        acc.push(`   ${key}: ${file1[key]}`);
-      }
-      if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
-        acc.push(` - ${key}: ${file1[key]}`);
-      }
-      if (!Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
-        acc.push(` + ${key}: ${file2[key]}`);
-      }
-      if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key) && file1[key] !== file2[key]) {
-        acc.push(` - ${key}: ${file1[key]}`);
-        acc.push(` + ${key}: ${file2[key]}`);
+  const sortedKeys = [...keys1, ...keys2]
+    .reduce((acc, key) => {
+      if (!acc.includes(key)) {
+        acc.push(key);
       }
       return acc;
-    }, []);
+    }, [])
+    .sort((a, b) => {
+      if (a.toLowerCase() < b.toLowerCase()) {
+        return -1;
+      }
+      if (a.toLowerCase() > b.toLowerCase()) {
+        return 1;
+      }
+      return 0;
+    });
 
-    const resultedStr = `{\n${resultedArr.join('\n')}\n}`;
-    console.log(resultedStr);
-  });
+  const resultedArr = sortedKeys.reduce((acc, key) => {
+    if (file1[key] === file2[key]) {
+      acc.push(`   ${key}: ${file1[key]}`);
+    }
+    if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
+      acc.push(` - ${key}: ${file1[key]}`);
+    }
+    if (!Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
+      acc.push(` + ${key}: ${file2[key]}`);
+    }
+    if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key) && file1[key] !== file2[key]) {
+      acc.push(` - ${key}: ${file1[key]}`);
+      acc.push(` + ${key}: ${file2[key]}`);
+    }
+    return acc;
+  }, []);
 
-program.parse();
+  const resultedStr = `{\n${resultedArr.join('\n')}\n}`;
+  console.log(resultedStr);
+  return resultedStr;
 };
 
-export default mainLogic;
+export default genDiff;
