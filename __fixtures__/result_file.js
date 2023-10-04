60ab7c466d96b83e0c2fa899a570a2c31bc8eb7a
Kirill <faeeeee@gmail.com>
2023-10-04 12:33:42 +0500

    add tests for .yml files

--- __fixtures__/result_file.js
+++ __fixtures__/result_file.js
@@ -1,4 +1,4 @@
-const result1 = `{
+const jsonResult1 = `{
  + another: extra line
    extra: line
  - follow: false
@@ -8,7 +8,7 @@ const result1 = `{
    timeout: 50
 }`;
 
-const result2 = `{
+const jsonResult2 = `{
  - another: extra line
  - extra: line
  - follow: true
@@ -16,8 +16,34 @@ const result2 = `{
  - timeout: 50
 }`;
 
-const result3 = `{
+const jsonResult3 = `{
 
 }`;
 
-export { result1, result2, result3 };
+const yamlResult4 = `{
+ + another: extra line
+   extra: line
+ - follow: false
+ + follow: true
+   host: gotta be same
+   proxy: 123.234.53.22
+ - timeout: 80
+ + timeout: 50
+}`;
+
+const yamlResult5 = `{
+ - another: extra line
+ - extra: line
+ - follow: true
+ - host: gotta be same
+ - proxy: 123.234.53.22
+ - timeout: 50
+}`;
+
+const yamlResult6 = `{
+
+}`;
+
+export {
+  jsonResult1, jsonResult2, jsonResult3, yamlResult4, yamlResult5, yamlResult6,
+};
--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -1,28 +1,51 @@
-import { getFixturePath } from '../helpers.js';
+import getFixturePath from '../helpers.js';
 import genDiff from '../index.js';
-import { result1, result2, result3 } from '../__fixtures__/result_file.js';
+import {
+  jsonResult1, jsonResult2, jsonResult3, yamlResult4, yamlResult5, yamlResult6,
+} from '../__fixtures__/result_file.js';
 
-let testFile1;
-let testFile2;
-let testFile3;
+let jsonTestFile1;
+let jsonTestFile2;
+let jsonTestFile3;
+let yamlTestFile4;
+let yamlTestFile5;
+let yamlTestFile6;
 
 beforeAll(() => {
-  testFile1 = getFixturePath('test_file1.json');
-  testFile2 = getFixturePath('test_file2.json');
-  testFile3 = getFixturePath('test_file3.json');
+  jsonTestFile1 = getFixturePath('test_file1.json');
+  jsonTestFile2 = getFixturePath('test_file2.json');
+  jsonTestFile3 = getFixturePath('test_file3.json');
+  yamlTestFile4 = getFixturePath('test_file4.yml');
+  yamlTestFile5 = getFixturePath('test_file5.yml');
+  yamlTestFile6 = getFixturePath('test_file6.yml');
 });
 
-test('both files have common and different data', () => {
-  const result = result1;
-  expect(genDiff(testFile1, testFile2)).toEqual(result);
+test('both json.files have common and different data', () => {
+  const result = jsonResult1;
+  expect(genDiff(jsonTestFile1, jsonTestFile2)).toEqual(result);
 });
 
-test('compare empty and non-empty files', () => {
-  const result = result2;
-  expect(genDiff(testFile2, testFile3)).toEqual(result);
+test('compare empty and non-empty json.files', () => {
+  const result = jsonResult2;
+  expect(genDiff(jsonTestFile2, jsonTestFile3)).toEqual(result);
 });
 
-test('compare two empty files', () => {
-  const result = result3;
-  expect(genDiff(testFile3, testFile3)).toEqual(result);
+test('compare two empty json.files', () => {
+  const result = jsonResult3;
+  expect(genDiff(jsonTestFile3, jsonTestFile3)).toEqual(result);
+});
+
+test('both yml.files have common and different data', () => {
+  const result = yamlResult4;
+  expect(genDiff(yamlTestFile4, yamlTestFile5)).toEqual(result);
+});
+
+test('compare empty and non-empty yml.files', () => {
+  const result = yamlResult5;
+  expect(genDiff(yamlTestFile5, yamlTestFile6)).toEqual(result);
+});
+
+test('compare two empty yml.files', () => {
+  const result = yamlResult6;
+  expect(genDiff(yamlTestFile6, yamlTestFile6)).toEqual(result);
 });
