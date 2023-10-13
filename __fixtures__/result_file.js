3703fb81f40b26acae57392a8b2a3fedf28f5f12
Kirill <faeeeee@gmail.com>
2023-10-13 17:01:16 +0500

    add new result of test

--- __fixtures__/result_file.js
+++ __fixtures__/result_file.js
@@ -148,6 +148,33 @@ const yamlResult5 = `{
 const yamlResult6 = `{
 }`;
 
+const jsonResult7 = `Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]`;
+
+const yamlResult8 = `Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]`;
+
 export {
-  jsonResult1, jsonResult2, jsonResult3, yamlResult4, yamlResult5, yamlResult6,
+  jsonResult1, jsonResult2,
+  jsonResult3, yamlResult4,
+  yamlResult5, yamlResult6,
+  jsonResult7, yamlResult8,
 };
