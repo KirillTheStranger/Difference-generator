b9b2bf40d1890e4410bf8efef372bf8e33729134
Kirill <faeeeee@gmail.com>
2023-10-16 13:03:08 +0500

    add tests for json format and error, rename test files

--- __fixtures__/result_file.js
+++ __fixtures__/result_file.js
@@ -1,4 +1,4 @@
-const jsonResult1 = `{
+export const stylishCompareTwoNonEmptyFiles = `{
     common: {
       + follow: false
         setting1: Value 1
@@ -43,7 +43,7 @@ const jsonResult1 = `{
     }
 }`;
 
-const jsonResult2 = `{
+export const stylishCompareEmptyWithNonEmptyFile = `{
   - common: {
         setting1: Value 1
         setting2: 200
@@ -70,85 +70,10 @@ const jsonResult2 = `{
     }
 }`;
 
-const jsonResult3 = `{
+export const compareTwoEmptyFiles = `{
 }`;
 
-const yamlResult4 = `{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}`;
-
-const yamlResult5 = `{
-  - common: {
-        setting1: Value 1
-        setting2: 200
-        setting3: true
-        setting6: {
-            key: value
-            doge: {
-                wow: 
-            }
-        }
-    }
-  - group1: {
-        baz: bas
-        foo: bar
-        nest: {
-            key: value
-        }
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-}`;
-
-const yamlResult6 = `{
-}`;
-
-const jsonResult7 = `Property 'common.follow' was added with value: false
+export const plainCompareTwoNonEmptyFiles = `Property 'common.follow' was added with value: false
 Property 'common.setting2' was removed
 Property 'common.setting3' was updated. From true to null
 Property 'common.setting4' was added with value: 'blah blah'
@@ -160,21 +85,119 @@ Property 'group1.nest' was updated. From [complex value] to 'str'
 Property 'group2' was removed
 Property 'group3' was added with value: [complex value]`;
 
-const yamlResult8 = `Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]`;
-
-export {
-  jsonResult1, jsonResult2,
-  jsonResult3, yamlResult4,
-  yamlResult5, yamlResult6,
-  jsonResult7, yamlResult8,
-};
+export const jsonCompareTwoNonEmptyFiles = `[
+ {
+  "key": "common",
+  "type": "nested",
+  "children": [
+   {
+    "key": "follow",
+    "type": "added",
+    "value2": false
+   },
+   {
+    "key": "setting1",
+    "type": "unchanged",
+    "value1": "Value 1"
+   },
+   {
+    "key": "setting2",
+    "type": "removed",
+    "value1": 200
+   },
+   {
+    "key": "setting3",
+    "type": "updated",
+    "value1": true,
+    "value2": null
+   },
+   {
+    "key": "setting4",
+    "type": "added",
+    "value2": "blah blah"
+   },
+   {
+    "key": "setting5",
+    "type": "added",
+    "value2": {
+     "key5": "value5"
+    }
+   },
+   {
+    "key": "setting6",
+    "type": "nested",
+    "children": [
+     {
+      "key": "doge",
+      "type": "nested",
+      "children": [
+       {
+        "key": "wow",
+        "type": "updated",
+        "value1": "",
+        "value2": "so much"
+       }
+      ]
+     },
+     {
+      "key": "key",
+      "type": "unchanged",
+      "value1": "value"
+     },
+     {
+      "key": "ops",
+      "type": "added",
+      "value2": "vops"
+     }
+    ]
+   }
+  ]
+ },
+ {
+  "key": "group1",
+  "type": "nested",
+  "children": [
+   {
+    "key": "baz",
+    "type": "updated",
+    "value1": "bas",
+    "value2": "bars"
+   },
+   {
+    "key": "foo",
+    "type": "unchanged",
+    "value1": "bar"
+   },
+   {
+    "key": "nest",
+    "type": "updated",
+    "value1": {
+     "key": "value"
+    },
+    "value2": "str"
+   }
+  ]
+ },
+ {
+  "key": "group2",
+  "type": "removed",
+  "value1": {
+   "abc": 12345,
+   "deep": {
+    "id": 45
+   }
+  }
+ },
+ {
+  "key": "group3",
+  "type": "added",
+  "value2": {
+   "deep": {
+    "id": {
+     "number": 45
+    }
+   },
+   "fee": 100500
+  }
+ }
+]`;
--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -1,68 +1,82 @@
 import { getFixturePath } from '../src/helpers.js';
 import genDiff from '../src/index.js';
 import {
-  jsonResult1, jsonResult2,
-  jsonResult3, yamlResult4,
-  yamlResult5, yamlResult6,
-  jsonResult7, yamlResult8,
+  stylishCompareTwoNonEmptyFiles,
+  stylishCompareEmptyWithNonEmptyFile,
+  compareTwoEmptyFiles,
+  plainCompareTwoNonEmptyFiles,
+  jsonCompareTwoNonEmptyFiles,
 } from '../__fixtures__/result_file.js';
 
-let jsonTestFile1;
-let jsonTestFile2;
-let jsonTestFile3;
-let yamlTestFile4;
-let yamlTestFile5;
-let yamlTestFile6;
+let pathOfJsonTestFile1;
+let pathOfJsonTestFile2;
+let pathOfJsonTestFile3;
+let pathOfYamlTestFile4;
+let pathOfYamlTestFile5;
+let pathOfYamlTestFile6;
 let stylishFormat;
 let plainFormat;
+let jsonFormat;
+let uknownFormat;
 
 beforeAll(() => {
-  jsonTestFile1 = getFixturePath('file1.json');
-  jsonTestFile2 = getFixturePath('file2.json');
-  jsonTestFile3 = getFixturePath('file3.json');
-  yamlTestFile4 = getFixturePath('file4.yml');
-  yamlTestFile5 = getFixturePath('file5.yml');
-  yamlTestFile6 = getFixturePath('file6.yml');
-  stylishFormat = { format: 'stylish' };
-  plainFormat = { format: 'plain' };
+  pathOfJsonTestFile1 = getFixturePath('file1.json');
+  pathOfJsonTestFile2 = getFixturePath('file2.json');
+  pathOfJsonTestFile3 = getFixturePath('file3.json');
+  pathOfYamlTestFile4 = getFixturePath('file4.yml');
+  pathOfYamlTestFile5 = getFixturePath('file5.yml');
+  pathOfYamlTestFile6 = getFixturePath('file6.yml');
+  stylishFormat = 'stylish';
+  plainFormat = 'plain';
+  jsonFormat = 'json';
+  uknownFormat = 'oops';
 });
 
 test('both json.files have common and different data, stylish format', () => {
-  const result = jsonResult1;
-  expect(genDiff(jsonTestFile1, jsonTestFile2, stylishFormat)).toEqual(result);
+  const result = stylishCompareTwoNonEmptyFiles;
+  expect(genDiff(pathOfJsonTestFile1, pathOfJsonTestFile2, stylishFormat)).toEqual(result);
 });
 
 test('compare empty and non-empty json.files, stylish format', () => {
-  const result = jsonResult2;
-  expect(genDiff(jsonTestFile1, jsonTestFile3, stylishFormat)).toEqual(result);
+  const result = stylishCompareEmptyWithNonEmptyFile;
+  expect(genDiff(pathOfJsonTestFile1, pathOfJsonTestFile3, stylishFormat)).toEqual(result);
 });
 
 test('compare two empty json.files, stylish format', () => {
-  const result = jsonResult3;
-  expect(genDiff(jsonTestFile3, jsonTestFile3, stylishFormat)).toEqual(result);
+  const result = compareTwoEmptyFiles;
+  expect(genDiff(pathOfJsonTestFile3, pathOfJsonTestFile3, stylishFormat)).toEqual(result);
 });
 
 test('both yml.files have common and different data, stylish format', () => {
-  const result = yamlResult4;
-  expect(genDiff(yamlTestFile4, yamlTestFile5, stylishFormat)).toEqual(result);
+  const result = stylishCompareTwoNonEmptyFiles;
+  expect(genDiff(pathOfYamlTestFile4, pathOfYamlTestFile5, stylishFormat)).toEqual(result);
 });
 
 test('compare empty and non-empty yml.files, stylish format', () => {
-  const result = yamlResult5;
-  expect(genDiff(yamlTestFile4, yamlTestFile6, stylishFormat)).toEqual(result);
+  const result = stylishCompareEmptyWithNonEmptyFile;
+  expect(genDiff(pathOfYamlTestFile4, pathOfYamlTestFile6, stylishFormat)).toEqual(result);
 });
 
 test('compare two empty yml.files, stylish format', () => {
-  const result = yamlResult6;
-  expect(genDiff(yamlTestFile6, yamlTestFile6, stylishFormat)).toEqual(result);
+  const result = compareTwoEmptyFiles;
+  expect(genDiff(pathOfYamlTestFile6, pathOfYamlTestFile6, stylishFormat)).toEqual(result);
 });
 
 test('compare two empty yml.files, plain format', () => {
-  const result = jsonResult7;
-  expect(genDiff(jsonTestFile1, jsonTestFile2, plainFormat)).toEqual(result);
+  const result = plainCompareTwoNonEmptyFiles;
+  expect(genDiff(pathOfJsonTestFile1, pathOfJsonTestFile2, plainFormat)).toEqual(result);
 });
 
 test('compare two empty yml.files, plain format', () => {
-  const result = yamlResult8;
-  expect(genDiff(yamlTestFile4, yamlTestFile5, plainFormat)).toEqual(result);
+  const result = plainCompareTwoNonEmptyFiles;
+  expect(genDiff(pathOfYamlTestFile4, pathOfYamlTestFile5, plainFormat)).toEqual(result);
+});
+
+test('both json.files have common and different data, json format', () => {
+  const result = jsonCompareTwoNonEmptyFiles;
+  expect(genDiff(pathOfJsonTestFile1, pathOfJsonTestFile2, jsonFormat)).toEqual(result);
+});
+
+test('Unknown format', () => {
+  expect(() => genDiff(pathOfJsonTestFile1, pathOfJsonTestFile1, uknownFormat)).toThrow(Error);
 });
