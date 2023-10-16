22b0fb82ed6a8886fb257db0acb8c605b87453c5
Kirill <faeeeee@gmail.com>
2023-10-16 11:58:59 +0500

    rework logic of chosing formatters

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,23 @@
+import _ from 'lodash';
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js';
 
 const chooseFormatterFrom = (data) => {
-  if (data.format === 'plain') {
+  if (_.isObject(data) && data.format === 'plain') {
     return plain;
+  }
+
+  if (_.isObject(data) && data.format === 'json') {
+    return json;
+  }
+
+  if (data === 'plain') {
+    return plain;
+  }
+
+  if (data === 'json') {
+    return json;
   }
 
   return stylish;
