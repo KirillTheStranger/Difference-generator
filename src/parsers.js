1ce85e7558c2556240d1bab1195e56e084c7cdeb
Kirill <faeeeee@gmail.com>
2023-10-18 13:22:52 +0500

    add abstract func makePath

--- src/parsers.js
+++ src/parsers.js
@@ -5,14 +5,15 @@ import yaml from 'js-yaml';
 const parseJson = (file) => JSON.parse(file);
 const parseYaml = (file) => yaml.load(file);
 
-const readFile = (filepath) => fs.readFileSync(filepath, 'utf8');
+const makePath = (filepath) => path.resolve(process.cwd(), filepath);
+const readFile = (filepath) => fs.readFileSync(makePath(filepath), 'utf8');
 const getFileExt = (filepath) => {
   const extention = filepath.includes('/') ? path.extname(filepath.split('/').at(-1)) : path.extname(filepath);
   return extention;
 };
 
 const parseFromPath = (filepath) => {
-  const fullPath = path.resolve(process.cwd(), filepath);
+  const fullPath = makePath(filepath);
   const extention = getFileExt(fullPath);
 
   if (extention === '.json') {
@@ -26,4 +27,4 @@ const parseFromPath = (filepath) => {
   return null;
 };
 
-export default parseFromPath;
+export { parseFromPath, readFile };
