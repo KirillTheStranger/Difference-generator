824af8092d7cfe6ae7a00a3de72e9c0a2e75ab01
Kirill <faeeeee@gmail.com>
2023-10-18 14:44:15 +0500

    rework test logic

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -3,84 +3,20 @@ import genDiff from '../src/index.js';
 import { readFile } from '../src/parsers.js';
 
 test.each([
-  ['Compare two json files', 'stylish', getFixturePath('file1.json'), getFixturePath('file2.json'), readFile(getFixturePath('result_files/stylishCompareTwoNonEmptyFiles.txt'))],
-  ['Compare empty and non-empty json files', 'stylish', getFixturePath('file1.json'), getFixturePath('file3.json'), readFile(getFixturePath('result_files/stylishCompareEmptyWithNonEmptyFile.txt'))],
-  ['Compare two empty json files', 'stylish', getFixturePath('file3.json'), getFixturePath('file3.json'), readFile(getFixturePath('result_files/compareTwoEmptyFiles.txt'))],
-])('%s with %s format', (nameOfTest, format, filepath1, filepath2, expectedResult) => {
-  expect(genDiff(filepath1, filepath2, format)).toEqual(expectedResult);
+  ['file1.json', 'file2.json', 'stylish', 'result_files/stylishCompareTwoFilesWithData.txt'],
+  ['file1.json', 'file3.json', 'stylish', 'result_files/stylishCompareEmptyWithNonEmptyFile.txt'],
+  ['file3.json', 'file3.json', 'stylish', 'result_files/compareTwoEmptyFiles.txt'],
+  ['file4.yml', 'file5.yml', 'stylish', 'result_files/stylishCompareTwoFilesWithData.txt'],
+  ['file4.yml', 'file6.yml', 'stylish', 'result_files/stylishCompareEmptyWithNonEmptyFile.txt'],
+  ['file6.yml', 'file6.yml', 'stylish', 'result_files/compareTwoEmptyFiles.txt'],
+  ['file1.json', 'file2.json', 'plain', 'result_files/plainCompareTwoFilesWithData.txt'],
+  ['file4.yml', 'file5.yml', 'plain', 'result_files/plainCompareTwoFilesWithData.txt'],
+  ['file1.json', 'file2.json', 'json', 'result_files/jsonCompareTwoFilesWithData.txt'],
+])('Compare %p and %p with %s format', (file1, file2, format, resultFile) => {
+  expect(genDiff(getFixturePath(file1), getFixturePath(file2), format)).toEqual(readFile(getFixturePath(resultFile)));
 });
 
-test('both json.files have common and different data, stylish format', () => {
-  const result = readFile(getFixturePath('result_files/stylishCompareTwoNonEmptyFiles.txt'));
-  const pathOfJsonTestFile1 = getFixturePath('file1.json');
-  const pathOfJsonTestFile2 = getFixturePath('file2.json');
-  const stylishFormat = 'stylish';
-  expect(genDiff(pathOfJsonTestFile1, pathOfJsonTestFile2, stylishFormat)).toEqual(result);
-});
-
-test('compare empty and non-empty json.files, stylish format', () => {
-  const result = readFile(getFixturePath('result_files/stylishCompareEmptyWithNonEmptyFile.txt'));
-  const pathOfJsonTestFile1 = getFixturePath('file1.json');
-  const pathOfJsonTestFile3 = getFixturePath('file3.json');
-  const stylishFormat = 'stylish';
-  expect(genDiff(pathOfJsonTestFile1, pathOfJsonTestFile3, stylishFormat)).toEqual(result);
-});
-
-test('compare two empty json.files, stylish format', () => {
-  const result = readFile(getFixturePath('result_files/compareTwoEmptyFiles.txt'));
-  const pathOfJsonTestFile3 = getFixturePath('file3.json');
-  const stylishFormat = 'stylish';
-  expect(genDiff(pathOfJsonTestFile3, pathOfJsonTestFile3, stylishFormat)).toEqual(result);
-});
-
-test('both yml.files have common and different data, stylish format', () => {
-  const result = readFile(getFixturePath('result_files/stylishCompareTwoNonEmptyFiles.txt'));
-  const pathOfYamlTestFile4 = getFixturePath('file4.yml');
-  const pathOfYamlTestFile5 = getFixturePath('file5.yml');
-  const stylishFormat = 'stylish';
-  expect(genDiff(pathOfYamlTestFile4, pathOfYamlTestFile5, stylishFormat)).toEqual(result);
-});
-
-test('compare empty and non-empty yml.files, stylish format', () => {
-  const result = readFile(getFixturePath('result_files/stylishCompareEmptyWithNonEmptyFile.txt'));
-  const pathOfYamlTestFile4 = getFixturePath('file4.yml');
-  const pathOfYamlTestFile6 = getFixturePath('file6.yml');
-  const stylishFormat = 'stylish';
-  expect(genDiff(pathOfYamlTestFile4, pathOfYamlTestFile6, stylishFormat)).toEqual(result);
-});
-
-test('compare two empty yml.files, stylish format', () => {
-  const result = readFile(getFixturePath('result_files/compareTwoEmptyFiles.txt'));
-  const pathOfYamlTestFile6 = getFixturePath('file6.yml');
-  const stylishFormat = 'stylish';
-  expect(genDiff(pathOfYamlTestFile6, pathOfYamlTestFile6, stylishFormat)).toEqual(result);
-});
-
-test('compare two empty json.files, plain format', () => {
-  const result = readFile(getFixturePath('result_files/plainCompareTwoNonEmptyFiles.txt'));
-  const pathOfJsonTestFile1 = getFixturePath('file1.json');
-  const pathOfJsonTestFile2 = getFixturePath('file2.json');
-  const plainFormat = 'plain';
-  expect(genDiff(pathOfJsonTestFile1, pathOfJsonTestFile2, plainFormat)).toEqual(result);
-});
-
-test('compare two empty yml.files, plain format', () => {
-  const result = readFile(getFixturePath('result_files/plainCompareTwoNonEmptyFiles.txt'));
-  const pathOfYamlTestFile4 = getFixturePath('file4.yml');
-  const pathOfYamlTestFile5 = getFixturePath('file5.yml');
-  const plainFormat = 'plain';
-  expect(genDiff(pathOfYamlTestFile4, pathOfYamlTestFile5, plainFormat)).toEqual(result);
-});
-
-test('both json.files have common and different data, json format', () => {
-  const result = readFile(getFixturePath('result_files/jsonCompareTwoNonEmptyFiles.txt'));
-  const pathOfJsonTestFile1 = getFixturePath('file1.json');
-  const pathOfJsonTestFile2 = getFixturePath('file2.json');
-  const jsonFormat = 'json';
-  expect(genDiff(pathOfJsonTestFile1, pathOfJsonTestFile2, jsonFormat)).toEqual(result);
-});
-
-test('Unknown format', () => {
+test('Compare two files with unknown format', () => {
   const pathOfJsonTestFile1 = getFixturePath('file1.json');
   const pathOfJsonTestFile2 = getFixturePath('file2.json');
   const uknownFormat = 'oops';
