edd59c34d8d515c62a5b795e59b3aee263ed390d
Kirill <faeeeee@gmail.com>
2023-10-19 11:30:01 +0500

    add object check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import { parseFromPath } from './parsers.js';
 import diffGenerator from './diffGenerator.js';
 import formatterSelector from './formatters/index.js';
@@ -5,9 +6,10 @@ import formatterSelector from './formatters/index.js';
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const contentOfFile1 = parseFromPath(filepath1);
   const contentOfFile2 = parseFromPath(filepath2);
+  const format = _.isObject(formatName) ? formatName.format : formatName;
 
   const typedData = diffGenerator(contentOfFile1, contentOfFile2);
-  const formatter = formatterSelector(formatName);
+  const formatter = formatterSelector(format);
   const result = formatter(typedData);
 
   return result;
