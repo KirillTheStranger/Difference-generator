2d1160bf594d24c309fea6cab529e864f0078027
Kirill <faeeeee@gmail.com>
2023-10-19 11:47:20 +0500

    remove object check

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import { parseFromPath } from './parsers.js';
 import diffGenerator from './diffGenerator.js';
 import formatterSelector from './formatters/index.js';
@@ -6,10 +5,9 @@ import formatterSelector from './formatters/index.js';
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const contentOfFile1 = parseFromPath(filepath1);
   const contentOfFile2 = parseFromPath(filepath2);
-  const format = _.isObject(formatName) ? formatName.format : formatName;
 
   const typedData = diffGenerator(contentOfFile1, contentOfFile2);
-  const formatter = formatterSelector(format);
+  const formatter = formatterSelector(formatName);
   const result = formatter(typedData);
 
   return result;
