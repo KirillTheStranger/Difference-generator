d5bbd175fbb21d18954f16fedcf9dda5e2d19eee
Kirill <faeeeee@gmail.com>
2023-10-19 12:56:15 +0500

    remove linter errors

--- src/parsers.js
+++ src/parsers.js
@@ -2,11 +2,12 @@ import * as fs from 'node:fs';
 import path from 'node:path';
 import yaml from 'js-yaml';
 
+const makePath = (filepath) => path.resolve(process.cwd(), filepath);
+const readFile = (filepath) => fs.readFileSync(makePath(filepath), 'utf8');
+
 const parseJson = (filepath) => JSON.parse(readFile(filepath));
 const parseYaml = (filepath) => yaml.load(readFile(filepath));
 
-const makePath = (filepath) => path.resolve(process.cwd(), filepath);
-const readFile = (filepath) => fs.readFileSync(makePath(filepath), 'utf8');
 const getFileExt = (filepath) => {
   const extention = filepath.includes('/') ? path.extname(filepath.split('/').at(-1)) : path.extname(filepath);
   return extention;
