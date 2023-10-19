4d4fd66e2958ecfbdb03c5432ff6cd820b60f6cc
Kirill <faeeeee@gmail.com>
2023-10-19 13:07:05 +0500

    move stringify func to module level

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,29 +1,29 @@
 import _ from 'lodash';
 
-const stylish = (array, replacer = ' ', spacesCount = 4) => {
-  const stringify = (value, currentDepth, replaceValue = ' ', replacesCount = 4) => {
-    const iter = (currentValue, depth) => {
-      if (!_.isObject(currentValue)) {
-        return `${currentValue}`;
-      }
+const stringify = (value, currentDepth, replaceValue = ' ', replacesCount = 4) => {
+  const iter = (currentValue, depth) => {
+    if (!_.isObject(currentValue)) {
+      return `${currentValue}`;
+    }
 
-      const indentSize = (depth * replacesCount) + 4;
-      const currentIndent = replaceValue.repeat(indentSize);
-      const bracketIndent = replaceValue.repeat(indentSize - replacesCount);
-      const lines = Object
-        .entries(currentValue)
-        .map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
+    const indentSize = (depth * replacesCount) + 4;
+    const currentIndent = replaceValue.repeat(indentSize);
+    const bracketIndent = replaceValue.repeat(indentSize - replacesCount);
+    const lines = Object
+      .entries(currentValue)
+      .map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
 
-      return [
-        '{',
-        ...lines,
-        `${bracketIndent}}`,
-      ].join('\n');
-    };
-
-    return iter(value, currentDepth);
+    return [
+      '{',
+      ...lines,
+      `${bracketIndent}}`,
+    ].join('\n');
   };
 
+  return iter(value, currentDepth);
+};
+
+const stylish = (array, replacer = ' ', spacesCount = 4) => {
   const iter = (arr, depth) => {
     const indentSize = (depth * spacesCount) - 2;
     const currentIndent = replacer.repeat(indentSize);
