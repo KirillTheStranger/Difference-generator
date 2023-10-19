ea6b8f830ee03de6977f8c7ee44c257e67e88c29
Kirill <faeeeee@gmail.com>
2023-10-19 14:49:18 +0500

    change argument

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import _ from 'lodash';
 import { program } from 'commander';
 import genDiff from '../src/index.js';
 
@@ -11,8 +10,7 @@ program
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const formatName = _.isObject(program.opts()) ? program.opts().format : program.opts();
-    console.log(genDiff(filepath1, filepath2, formatName));
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
 
 program.parse();
