07e1b85fd63d4c774d75ff387eac1e6cdb4b5696
Kirill <faeeeee@gmail.com>
2023-10-23 12:20:57 +0500

    rework inside logic of getting data

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import { parseFromPath, readFile, getFileExt } from './parsers.js';
 import diffGenerator from './diffGenerator.js';
 import formatter from './formatters/index.js';
 
-const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const [data1, extantion1] = [readFile(filepath1), getFileExt(filepath1)];
-  const [data2, extantion2] = [readFile(filepath2), getFileExt(filepath2)];
-  const contentOfFile1 = parseFromPath(data1, extantion1);
-  const contentOfFile2 = parseFromPath(data2, extantion2);
+const getData = (filepath) => {
+  const extension = getFileExt(filepath);
+  const content = readFile(filepath);
+  const data = parseFromPath(content, extension);
+  return data;
+};
 
-  const typedData = diffGenerator(contentOfFile1, contentOfFile2);
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+  const data1 = getData(filepath1);
+  const data2 = getData(filepath2);
+
+  const typedData = diffGenerator(data1, data2);
   const result = formatter(typedData, formatName);
 
   return result;
