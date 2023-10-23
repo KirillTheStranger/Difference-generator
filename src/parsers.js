8e72ca454e9b689110aa9125cb3028c04f097d62
Kirill <faeeeee@gmail.com>
2023-10-23 12:53:13 +0500

    change switch structure

--- src/parsers.js
+++ src/parsers.js
@@ -5,7 +5,6 @@ const parseFromPath = (data, extention) => {
     case 'json':
       return JSON.parse(data);
     case 'yaml':
-      return yaml.load(data);
     case 'yml':
       return yaml.load(data);
     default:
