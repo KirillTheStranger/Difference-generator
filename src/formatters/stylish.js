60f314efec690d082475d8c6b3e946b825cae80e
Kirill <faeeeee@gmail.com>
2023-10-23 15:58:22 +0500

    remove comments

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,25 +1,15 @@
 import _ from 'lodash';
 
-// const getIndent = (depth, replaceValue, spaceCount) => {
-//   const indentSize = (depth * spaceCount) - 2;
-//   const currentIndent = replaceValue.repeat(indentSize);
-//   const bracketIndent = replaceValue.repeat(indentSize - spaceCount + 2);
-//   return [currentIndent, bracketIndent];
-// };
-
 const getIndent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount - 2);
 
 const stringify = (data, depth) => {
-  const iter = (currentValue, currentDepth, replaceValue = ' ', spaceCount = 4) => {
+  const iter = (currentValue, currentDepth) => {
     if (!_.isObject(currentValue)) {
       return `${currentValue}`;
     }
 
-    // const indentSize = (currentDepth * spaceCount) + 4;
-    // const currentIndent = replaceValue.repeat(indentSize);
-    // const bracketIndent = replaceValue.repeat(indentSize - spaceCount);
     const currentIndent = getIndent(currentDepth);
-    const bracketIndent = getIndent(currentDepth);
+    const bracketIndent = getIndent(currentDepth + 0.5);
 
     const lines = Object
       .entries(currentValue)
@@ -36,10 +26,10 @@ const stringify = (data, depth) => {
 };
 
 const stylish = (data) => {
-  const iter = (iterData, depth, replaceValue = ' ', spacesCount = 4) => {
+  const iter = (iterData, depth) => {
     const currentIndent = getIndent(depth);
-    const bracketIndent = getIndent(depth);
-    // const [currentIndent, bracketIndent] = getIndent(depth, replaceValue, spacesCount);
+    const bracketIndent = getIndent(depth - 0.5);
+
     const result = iterData.map(({
       key, type, value1, value2, children,
     }) => {
