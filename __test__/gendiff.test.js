6abb3785e60484d523f2742cb0610f65ddf9c078
Kirill <faeeeee@gmail.com>
2023-10-25 11:12:10 +0500

    remove tests of comparing with empty file, remove result file

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -3,10 +3,8 @@ import genDiff, { readFile } from '../src/index.js';
 
 test.each([
   ['file1.json', 'file2.json', 'result_files/result_stylish.txt', 'stylish'],
-  ['file1.json', 'file3.json', 'result_files/result_stylish_compare_with_empty.txt', 'stylish'],
   ['file1.json', 'file2.json', 'result_files/result_stylish.txt'],
   ['file1.yml', 'file2.yml', 'result_files/result_stylish.txt', 'stylish'],
-  ['file1.yml', 'file3.yml', 'result_files/result_stylish_compare_with_empty.txt', 'stylish'],
   ['file1.yml', 'file2.yml', 'result_files/result_stylish.txt'],
   ['file1.json', 'file2.json', 'result_files/result_plain.txt', 'plain'],
   ['file1.yml', 'file2.yml', 'result_files/result_plain.txt', 'plain'],
