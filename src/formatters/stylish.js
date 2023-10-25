006d28c71a8b939b252f3584be4b1fd728469ed1
Kirill <faeeeee@gmail.com>
2023-10-25 11:17:08 +0500

    rename argument of stringify func

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,18 +2,18 @@ import _ from 'lodash';
 
 const getIndent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount - 2);
 
-const stringify = (data, depth) => {
-  const iter = (currentValue, currentDepth) => {
+const stringify = (data, currentDepth) => {
+  const iter = (currentValue, depth) => {
     if (!_.isObject(currentValue)) {
       return `${currentValue}`;
     }
 
-    const currentIndent = getIndent(currentDepth);
-    const bracketIndent = getIndent(currentDepth);
+    const currentIndent = getIndent(depth);
+    const bracketIndent = getIndent(depth);
 
     const lines = Object
       .entries(currentValue)
-      .map(([key, val]) => `${currentIndent}      ${key}: ${iter(val, currentDepth + 1)}`);
+      .map(([key, val]) => `${currentIndent}      ${key}: ${iter(val, depth + 1)}`);
 
     return [
       '{',
@@ -22,7 +22,7 @@ const stringify = (data, depth) => {
     ].join('\n');
   };
 
-  return iter(data, depth);
+  return iter(data, currentDepth);
 };
 
 const stylish = (data) => {
@@ -41,7 +41,7 @@ const stylish = (data) => {
         case 'unchanged':
           return `${currentIndent}  ${content.key}: ${stringify(content.value, depth)}`;
         case 'updated':
-          return `${currentIndent}- ${content.key}: ${stringify(content.value1, depth)}\n${currentIndent}+ ${content.key}: ${stringify(content.value2, depth)}`;
+          return [`${currentIndent}- ${content.key}: ${stringify(content.value1, depth)}`, `${currentIndent}+ ${content.key}: ${stringify(content.value2, depth)}`].join('\n');
         default:
           throw new Error(`Uknown type: ${content.type}`);
       }
